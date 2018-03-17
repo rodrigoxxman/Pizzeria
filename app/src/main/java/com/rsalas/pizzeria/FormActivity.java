4b1ee324fed9f4ff98233a98a8e641d5db62da8e
Rodrigo M. Salas <rodrigo.salas@tecsup.edu.pe>
2018-03-16 23:28:53 -0500

    2do commit

--- app/src/main/java/com/rsalas/pizzeria/FormActivity.java
+++ app/src/main/java/com/rsalas/pizzeria/FormActivity.java
@@ -1,13 +1,73 @@
 package com.rsalas.pizzeria;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 
 public class FormActivity extends AppCompatActivity {
+    private Spinner spinner1;
+    private RadioGroup radiogroup;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner);
+        radiogroup=(RadioGroup) findViewById(R.id.Tiposdemasa);
+        checkBox=(CheckBox)findViewById(R.id.comp1);
+        checkBox.setChecked(true);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                Toast.makeText(parent.getContext(),
+                        "Haz seleccionado la pizza : " + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg) {
+            }
+        });
+    }
+    public void showDialog(View view){
+
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_dialog);
+        dialog.setTitle("Orden");
+
+        Button dialogButtonCancel = (Button) dialog.findViewById(R.id.customDialogCancel);
+        Button dialogButtonOk = (Button) dialog.findViewById(R.id.customDialogOk);
+        dialogButtonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "proceso cancelado!", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        // Your android custom dialog ok action
+        // Action for custom dialog ok button click
+        dialogButtonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "pedido procesado!", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
     }
 }
+
